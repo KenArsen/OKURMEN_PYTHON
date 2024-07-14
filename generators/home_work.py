1ff90c2340e9fba8d7955fca6d9f77173d7cedc3
KenArsen <kenzhegulov2002@gmail.com>
2024-07-14 20:49:57 +0600

    Added home work

--- generators/home_work.py
+++ generators/home_work.py
@@ -10,6 +10,9 @@ map функциясынын жардамы менен аткарыныздар
 Жооп:
 5 6 8 11 10 0
 """
+numbers = list(map(int, input().split()))
+print(list(map(lambda x: x if x > 0 else -x, numbers)))
+print(list(map(lambda x: abs(x), numbers)))
 
 
 # 2 - тапшырма
@@ -22,6 +25,9 @@ key_1=value_1 key_2=value_2 .... key_n=value_n
 деп кортеж тибинде чыгарыныздар.
 map функциясынын жардамы менен аткарыныздар
 """
+
+text = list(map(str, input().split()))
+print(tuple(map(lambda x: tuple(x.split("=")), text)))
 
 
 # 3 - тапшырма
@@ -37,3 +43,8 @@ map функциясынын жардамы менен аткарыныздар
 Жоопко:
 11 -23
 """
+
+numbers = list(map(int, input().split()))
+print(list(filter(lambda x: 10 <= x < 100, numbers)))
+# x >= 10 and x < 100
+# 10 <= x < 100
